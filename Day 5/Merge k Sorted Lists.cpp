e053b969675324baff431bbfea988b522c44f65c
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-09-15 22:33:03 +0530

    Added file

--- Day 5/Merge k Sorted Lists.cpp
+++ Day 5/Merge k Sorted Lists.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-/* --------Time complexity: O(n*k), Space complexity: O(n*k) --------*/
+// -----------APPROACH 1 ------------// 
+
+/* --------Time complexity: O(n), Space complexity: O(n) --------*/
 
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         int index=-1;
@@ -32,5 +34,43 @@ public:
             temp = temp->next;
         }
         return dummy;
+    }
+};
+// ---------------APPROACH 2  ------------------// 
+
+class Solution {
+    public:
+/* ------    TIME COMPLEXITY ~ O(NLogK), SPACE COMPLEXITY: O(K)  ---------    */
+
+    class compare{      // custom comparator for comparing ListNode vals
+        public:
+        bool operator()(ListNode* first,ListNode* second ) {
+            return first->val > second->val;}
+    };
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, compare> minHeap;
+        ListNode* head = NULL;
+        ListNode* tail = NULL;
+
+        for(int i=0; i < lists.size(); i++){
+            if(lists[i] != NULL)
+            minHeap.push(lists[i]);
+        }
+        while(minHeap.size() > 0){
+            ListNode* top = minHeap.top();
+            minHeap.pop();
+            if(top->next != NULL){
+                minHeap.push(top->next);
+            }
+            if(head == NULL){   
+                head = top;
+                tail = top;
+            }
+            else{
+                tail -> next = top;
+                tail = top;
+            }
+        }
+        return head;
     }
 };
